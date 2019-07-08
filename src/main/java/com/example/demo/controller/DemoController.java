6d48003a3a18e04fb2d9d8eb6d152e9bdfb47181
sergey.zubarev <sergey.zubarev@leroymerlin.ru>
2019-07-08 14:41:50 +0300

    first commit

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        var result = "java version: " + System.getProperty("java.version");
+        return result;
+    }
+}
